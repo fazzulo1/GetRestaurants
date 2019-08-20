84a03b2b6f9ae478626df30b4eea6464646654a5
haroldjohan <haroldmiranda1@Harolds-MacBook-Air.local>
2019-08-20 14:07:39 -0400

    added seed data and create function

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const app = express ();
+const app = express();
 const Port = 3003;
 
-
-const restaurantController = require('./controllers/restaurantcontroller')
+const restaurantController = require('./controllers/restaurantcontroller');
 // Middleware
 app.use(express.json());
-
 
 // app.get('/', (req,res)=> {
 //     res.send('hello world')
 // })
-
-
 
 // CORS
 // const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
@@ -29,16 +25,28 @@ app.use(express.json());
 // }
 // app.use(cors(corsOptions))
 
-app.use('/restaurant', restaurantController)
+app.use('/restaurant', restaurantController);
+
+// seed
+const seed = require('./models/seed');
+const Restaurant = require('./models/restarauntSchema');
+app.get('/seed', (req, res) => {
+  Restaurant.create(seed, (err, createdRestaurant) => {
+    res.redirect('/restauntant');
+  });
+});
 
 // Error / Disconnection
-mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+mongoose.connection.on('error', err =>
+  console.log(err.message + ' is Mongod not running?')
+);
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-mongoose.connect('mongodb://localhost:27017/bookmark', { useNewUrlParser: true })
-mongoose.connection.once('open', ()=>{
-    console.log('connected to mongoose...')
-})
+mongoose.connect('mongodb://localhost:27017/bookmark', {
+  useNewUrlParser: true
+});
+mongoose.connection.once('open', () => {
+  console.log('connected to mongoose...');
+});
 
-
-app.listen(Port, () => console.log('Listening on port:',Port));
+app.listen(Port, () => console.log('Listening on port:', Port));
