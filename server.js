bdc4852b636c17c2b221423d2ab92ad0528a9dce
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-08-20 16:47:19 -0400

    updated schema and seed data

--- server.js
+++ server.js
@@ -13,17 +13,20 @@ app.use(express.json());
 // })
 
 // CORS
-// const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     }
-// }
-// app.use(cors(corsOptions))
+const whitelist = [
+  'http://localhost:3000',
+  'https://fathomless-sierra-68956.herokuapp.com'
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
 
 app.use('/restaurant', restaurantController);
 
@@ -42,7 +45,7 @@ mongoose.connection.on('error', err =>
 );
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-mongoose.connect('mongodb://localhost:27017/bookmark', {
+mongoose.connect('mongodb://localhost:27017/restaurant', {
   useNewUrlParser: true
 });
 mongoose.connection.once('open', () => {
