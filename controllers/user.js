eaef3ae8a2fc392ce755d6c44d19c9fd0a8ea115
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-08-23 17:03:47 -0400

    add to favorites works

--- controllers/user.js
+++ controllers/user.js
@@ -2,20 +2,20 @@ const express = require('express');
 const user = express.Router();
 const User = require('../models/user');
 
+// log in
 user.post('/login', (req, res) => {
-  console.log('got login', req.body);
   User.findOne({ username: req.body.username }, (err, foundUser) => {
     if (err) {
       res.status(400).json({ error: err.message });
     } else if (foundUser.password === req.body.password) {
-      console.log('found', foundUser);
-      res.status(200).json('Login good');
+      res.status(200).json(foundUser);
     } else {
       res.status(200).json('Not good');
     }
   });
 });
 
+// sign up
 user.post('/register', (req, res) => {
   User.create(req.body, (err, createdUser) => {
     if (err) {
@@ -24,6 +24,19 @@ user.post('/register', (req, res) => {
       });
     }
     res.status(200).json(createdUser);
+  });
+});
+
+// edit
+user.put('/:id', (req, res) => {
+  console.log(req.body);
+  User.findByIdAndUpdate(req.params.id, req.body, (err, updatedUser) => {
+    if (err) {
+      res.status(400).json({
+        error: err.message
+      });
+    }
+    res.status(200).json(updatedUser);
   });
 });
 
